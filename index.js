485e353562a7c7f8317b502c5839c88072542e96
Ej <101543580+secret2213@users.noreply.github.com>
2023-09-08 00:49:14 +0800

    commit

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ let currCity = "London";
 let units = "metric";
 
 // Selectors
-let city = document.querySelector(".weather__city");
-let datetime = document.querySelector(".weather__datetime");
-let weather__forecast = document.querySelector('.weather__forecast');
-let weather__temperature = document.querySelector(".weather__temperature");
-let weather__icon = document.querySelector(".weather__icon");
-let weather__minmax = document.querySelector(".weather__minmax")
-let weather__realfeel = document.querySelector('.weather__realfeel');
-let weather__humidity = document.querySelector('.weather__humidity');
-let weather__wind = document.querySelector('.weather__wind');
-let weather__pressure = document.querySelector('.weather__pressure');
+let city = document.querySelector(".weather-city");
+let datetime = document.querySelector(".weather-datetime");
+let weatherForecast = document.querySelector('.weather-forecast');
+let weatherTemperature = document.querySelector(".weather-temperature");
+let weatherIcon = document.querySelector(".weather-icon");
+let weatherMinmax = document.querySelector(".weather-minmax")
+let weatherRealfeel = document.querySelector('.weather-realfeel');
+let weatherHumidity = document.querySelector('.weather-humidity');
+let weatherWind = document.querySelector('.weather-wind');
+let weatherPressure = document.querySelector('.weather-pressure');
 
 // search
-document.querySelector(".weather__search").addEventListener('submit', e => {
-    let search = document.querySelector(".weather__searchform");
+document.querySelector(".weather-search").addEventListener('submit', e => {
+    let search = document.querySelector(".weather-searchform");
     // prevent default action
     e.preventDefault();
     // change current city
@@ -28,7 +28,7 @@ document.querySelector(".weather__search").addEventListener('submit', e => {
 })
 
 // units
-document.querySelector(".weather_unit_celsius").addEventListener('click', () => {
+document.querySelector(".weather-unit-celsius").addEventListener('click', () => {
     if(units !== "metric"){
         // change to metric
         units = "metric"
@@ -37,7 +37,7 @@ document.querySelector(".weather_unit_celsius").addEventListener('click', () => 
     }
 })
 
-document.querySelector(".weather_unit_farenheit").addEventListener('click', () => {
+document.querySelector(".weather-unit-farenheit").addEventListener('click', () => {
     if(units !== "imperial"){
         // change to imperial
         units = "imperial"
@@ -76,18 +76,17 @@ function convertCountryCode(country){
 function getWeather(){
     const API_KEY = '9c1f6de9d01a67b86520f9f60c0a14b7'
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currCity}&appid=${API_KEY}&units=${units}`).then(res => res.json()).then(data => {
-    console.log(data)
-    city.innerHTML = `${data.name}, ${convertCountryCode(data.sys.country)}`
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currCity}&appid=${API_KEY}&units=${units}`).then(res => res.json()).then(data => 
+    {city.innerHTML = `${data.name}, ${convertCountryCode(data.sys.country)}`
     datetime.innerHTML = convertTimeStamp(data.dt, data.timezone); 
-    weather__forecast.innerHTML = `<p>${data.weather[0].main}`
-    weather__temperature.innerHTML = `${data.main.temp.toFixed()}&#176`
-    weather__icon.innerHTML = `   <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" />`
-    weather__minmax.innerHTML = `<p>Min: ${data.main.temp_min.toFixed()}&#176</p><p>Max: ${data.main.temp_max.toFixed()}&#176</p>`
-    weather__realfeel.innerHTML = `${data.main.feels_like.toFixed()}&#176`
-    weather__humidity.innerHTML = `${data.main.humidity}%`
-    weather__wind.innerHTML = `${data.wind.speed} ${units === "imperial" ? "mph": "m/s"}` 
-    weather__pressure.innerHTML = `${data.main.pressure} hPa`
+    weatherForecast.innerHTML = `<p>${data.weather[0].main}`
+    weatherTemperature.innerHTML = `${data.main.temp.toFixed()}&#176`
+    weatherIcon.innerHTML = `   <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" />`
+    weatherMinmax.innerHTML = `<p>Min: ${data.main.temp_min.toFixed()}&#176</p><p>Max: ${data.main.temp_max.toFixed()}&#176</p>`
+    weatherRealfeel.innerHTML = `${data.main.feels_like.toFixed()}&#176`
+    weatherHumidity.innerHTML = `${data.main.humidity}%`
+    weatherWind.innerHTML = `${data.wind.speed} ${units === "imperial" ? "mph": "m/s"}` 
+    weatherPressure.innerHTML = `${data.main.pressure} hPa`
 })
 }
 
