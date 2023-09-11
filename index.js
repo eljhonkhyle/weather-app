bab4f2e3825126e81a5007b0a05c755689ed0161
Ej <101543580+secret2213@users.noreply.github.com>
2023-09-12 05:08:02 +0800

    commit

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// darkmode
+const toggleSwitch = document.getElementById("moon")
+const body = document.querySelector('body')
+
+toggleSwitch.addEventListener('click', function(){
+this.classList.toggle('bi-sun-fill')
+
+
+if(this.classList.toggle('bi-brightness-low-fill')){
+    body.style.background = '#111111'
+    body.style.color = 'white'
+    body.style.transition = '1s'
+    
+}else{
+    (this.classList.toggle('bi-brightness-high-fill'))
+    body.style.background = 'white'
+    body.style.color = '#111111'
+    body.style.transition = '1s'
+}
+
+})
+
+
+
 // state
 let currCity = "Poblacion";
 let units = "metric";
